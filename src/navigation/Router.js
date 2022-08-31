7e7a9fd75dee01e891cf826fcb34b9428643d373
Thomas Austin <austint@chc.edu>
2022-08-31 14:02:33 -0400

    First Version of my Shopper React Native mobile application

---
+++ src/navigation/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/Home';
+import TabNavigator from './TabNavigator';
+
+const Stack = createStackNavigator();
+
+const Router = props => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'Home'}
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name={'Start Shopping!'} component={TabNavigator}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
