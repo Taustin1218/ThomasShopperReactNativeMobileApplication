16211414225713411579b915a5714ec81ca150c7
Thomas Austin <austint@chc.edu>
2022-09-25 21:32:49 -0400

    Sept 21

--- src/screens/Lists/index.js
+++ src/screens/Lists/index.js
@@ -1,29 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, FlatList } from 'react-native';
 import List from '../../components/List';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
+// import openDtabase hook
+import { openDatabase } from "react-native-sqlite-storage";
+
+// use hook
+const shopperDB = openDatabase({name: 'Shopper.db'});
+const listsTableName = 'Lists';
 
 const ListsScreen = props => {
 
   const navigation = useNavigation();
 
-  const [lists, setLists] = useState(
-    [
-      {
-        id: 1,
-        name: 'Grocery List',
-        store: 'Redners',
-        date: '2022-09-14',
-      },
-      {
-        id: 2,
-        name: 'Back To School List',
-        store: 'Staples',
-        date: '2022-09-15',
-      },
-    ]
-  );
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      // declare an empty array that will sstore the results of the
+      // SELECT
+      let results = [];
+      // declare a transaction that will execute the SELECT
+      shopperDB.transaction(txn => {
+        // execute SELECT
+        txn.executeSql(
+          `SELECT * FROM ${listsTableName}`,
+          [],
+          // callback fucntion to handle the results from the
+          // SELECT
+          (_, res) => {
+            // get number of rows of data selected
+            let len = res.rows.length;
+            console.log('Length of lists ' + len);
+            // if more than one row was returned
+            if (len > 0){
+              // loop through the rows
+              for (let i = 0; i < len; i++){
+                // push a row of data at a time onto the
+                // results array
+                let item = res.rows.items(i);
+                results.push({
+                  id: item.id,
+                  name: item.name,
+                  store: item.store,
+                  date: item.date,
+                });
+              }
+              // assign results array to lists state variable
+              setLists(results);
+            } else {
+              // if no rows of data were returned,
+              // set lists state variable to an empty array
+              setLists([]);
+            }
+          },
+          error => {
+            console.log('Error getting lists ' + error.mesage);
+          },
+        )
+      }); 
+   });
+   return listener;
+  });
 
   return (
     <View style={styles.container}>
@@ -31,6 +70,7 @@ const ListsScreen = props => {
         <FlatList
           data={lists}
           renderItem={({item}) => <List post={item} />}
+          keyExtractor={item => item.id}
         />
       </View>
         <View style={styles.bottom}>
